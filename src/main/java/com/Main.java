b47d3a4be482494315c60349570d025936619367
Sof <s.izegrar@student.maastrichtuniversity.nl>
2025-05-25 16:36:32 +0200

    to test

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,10 +5,15 @@ import com.data_management.DataStorage;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0 && args[0].equalsIgnoreCase("DataStorage")) {
-            DataStorage.main(new String[]{});
-        } else {
-            HealthDataSimulator.main(new String[]{});
+        try {
+            if (args.length > 0 && args[0].equals("DataStorage")) {
+                DataStorage.main(new String[]{});
+            } else {
+                HealthDataSimulator.main(args);
+            }
+        } catch (Exception e) {
+            System.err.println("An error occurred while running the simulation: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
